f2eb4e4b27cf3608ed4ba83a61b0ee110da8b89c
pau-torrente <ptbadia@gmail.com>
2025-01-29 13:37:30 +0100

    Added preliminary pyqsp code

--- Case 3 - Qpurpose/angleseq-modification.py
+++ Case 3 - Qpurpose/angleseq-modification.py
@@ -1,4 +1,7 @@
 import numpy as np
+import pyqsp
+from pyqsp import angle_sequence, response
+from pyqsp.poly import polynomial_generators, PolyTaylorSeries
 
 """
 The python package pyqsp allows you to generate new angle sequences: https://github.com/ichuang/pyqsp
@@ -13,15 +16,41 @@ However, for use in the QSVT algorithm, 'phiset' as output by the sample code:
 
 must be slightly modified. The function QSVT_format below implements the required
 modification. 
-"""	
+"""
+
 
 def QSVT_format(phiset):
-	n = len(phiset)-1
-	Phi = np.zeros(n)
-	Phi[1:n] = phiset[1:n]-np.pi/2
-	Phi[0] = phiset[0]+phiset[-1]+((n-2)%4)*np.pi/2
-	# If you use output from the MATLAB package qsppack you have to replace (n-2) by (n-1) in the above formula!
-	return Phi
-    
-    
-    
+    n = len(phiset) - 1
+    Phi = np.zeros(n)
+    Phi[1:n] = phiset[1:n] - np.pi / 2
+    Phi[0] = phiset[0] + phiset[-1] + ((n - 2) % 4) * np.pi / 2
+    # If you use output from the MATLAB package qsppack you have to replace (n-2) by (n-1) in the above formula!
+    return Phi
+
+
+def QSVT_cosinus(degree_cutoff: int, max_scale: float, M_step: int):
+    func = lambda x: max_scale * np.cos(M_step * x)
+
+    poly = PolyTaylorSeries().taylor_series(
+        func=func, degree=degree_cutoff, max_scale=max_scale, chebyshev_basis=True, cheb_samples=2 * degree_cutoff
+    )
+
+    angles, reduced_angles, parity = angle_sequence.QuantumSignalProcessingPhases(
+        poly, method="sym_qsp", chebyshev_basis=True
+    )
+
+    return QSVT_format(angles)
+
+
+def QSVT_sinus(degree_cutoff: int, max_scale: float, M_step: int):
+    func = lambda x: max_scale * np.sin(M_step * x)
+
+    poly = PolyTaylorSeries().taylor_series(
+        func=func, degree=degree_cutoff, max_scale=max_scale, chebyshev_basis=True, cheb_samples=2 * degree_cutoff
+    )
+
+    angles, reduced_angles, parity = angle_sequence.QuantumSignalProcessingPhases(
+        poly, method="sym_qsp", chebyshev_basis=True
+    )
+
+    return QSVT_format(angles)

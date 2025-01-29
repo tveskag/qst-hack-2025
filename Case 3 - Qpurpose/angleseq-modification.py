3c94bb6fc7072a610c365e4deba536d1d18b8e55
GOHelle <gardoh@imada.sdu.dk>
2025-01-29 12:23:08 +0100

    Update angleseq-modification.py
    Cleaned up the text slightly.

--- Case 3 - Qpurpose/angleseq-modification.py
+++ Case 3 - Qpurpose/angleseq-modification.py
@@ -1,21 +1,17 @@
 import numpy as np
 
 """
-The python package pyqsp
+The python package pyqsp allows you to generate new angle sequences: https://github.com/ichuang/pyqsp
 
-The sample code under "A guide within a guide" in the python package pyqsp
-
-https://github.com/ichuang/pyqsp
-
-will allow you to generate more angle sequences. However, for use in the QSVT algorithm, 
-'phiset' as output by the sample code: 
+The sample code under "A guide within a guide" is the most efficient way to get started.
+However, for use in the QSVT algorithm, 'phiset' as output by the sample code: 
 
 (phiset, red_phiset, parity) = angle_sequence.QuantumSignalProcessingPhases(
     poly,
     method='sym_qsp',
     chebyshev_basis=True)
 
-must be slightly modified. The function QSVT_format implements the required
+must be slightly modified. The function QSVT_format below implements the required
 modification. 
 """	
 
@@ -24,7 +20,7 @@ def QSVT_format(phiset):
 	Phi = np.zeros(n)
 	Phi[1:n] = phiset[1:n]-np.pi/2
 	Phi[0] = phiset[0]+phiset[-1]+((n-2)%4)*np.pi/2
-    # If you use output from the MATLAB package qsppack you have to replace (n-2) by (n-1) in the above formula!
+	# If you use output from the MATLAB package qsppack you have to replace (n-2) by (n-1) in the above formula!
 	return Phi
     
     
